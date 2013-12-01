0e4afa6d0971be6ae6bbd2c588a126bff9a1b9eb
unknown <benjaminstrawn@gmail.com>
2013-12-01 17:23:34 -0500

    slider maybe working right?

--- public/js/editing/editing.js
+++ public/js/editing/editing.js
@@ -4,8 +4,17 @@ angular.module('Editing', ['ui.bootstrap', 'World', 'Selection', 'Tiles'])
 
 	$scope.World = World;
 	$scope.dragging = false;
+	$scope.holding = false;
+	$scope.position = {};
+	$scope.widths = {};
 	$scope.rowStyle = {
 		width: $scope.world[0].length*33
+	};
+
+	$scope.init = function() {
+		var mapWidth = $('#map').width();
+		$('#viewportBox').width(mapWidth / 2 - 8);
+		$('#selectionBox').width(mapWidth / 2 - 8);
 	};
 
 	$scope.stopDrag = function() {
@@ -36,6 +45,40 @@ angular.module('Editing', ['ui.bootstrap', 'World', 'Selection', 'Tiles'])
 		var newTiles = TileFactory.generateTiles(SelectedTiles.selectedTiles);
 		World.setTiles(x, y, newTiles);
 	};
+
+
+	$scope.mapMove = function(e) {
+		if ($scope.holding) {
+			var xDiff = e.pageX - $scope.position.x;
+			$('#viewportBox').width($scope.widths.a + xDiff);
+			$('#selectionBox').width($scope.widths.b - xDiff);
+			// position = {x: e.pageX, y: e.pageY};
+
+			/*var mapWidth = $('#map').width();
+			var barWidth = $('#mover').width();
+			var viewWidth = $('#viewportBox').width();
+			var selectWidth = $('#selectionBox').width();
+			console.log(viewWidth, '+', selectWidth, '+', barWidth, '=', viewWidth + selectWidth + barWidth, '=', mapWidth, '?');*/
+		}
+	};
+
+	$scope.moverDown = function(e) {
+		$scope.holding = true;
+		$scope.position = {x: e.pageX, y: e.pageY};
+		var width = $('#viewportBox').width();
+		var width2 = $('#selectionBox').width();
+		$scope.widths = {a: width, b: width2};
+	};
+
+	$scope.moverUp = function() {
+		$scope.holding = false;
+	};
+
+	$scope.mapLeave = function() {
+		$scope.holding = false;
+	};
+
+	$scope.init();
 }])
 
 .directive('editTiles', [function() {
